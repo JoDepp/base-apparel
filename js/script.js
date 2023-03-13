96f3e6fe929a075ad433a5a8c855432e206ddc66
jody_d <jodydepp@gmail.com>
2023-03-13 12:11:36 -0400

    can't get commit to work

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('#email');
+
+const email = document.querySelector('.user-input');
+
+const emailError = document.querySelector('.error');
+
+const button = document.querySelector('.submit-button');
+
+function validateEmail(email) {
+    let res = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return res.test(email);
+  }
+  function validate() {
+    let result = $("#result");
+    let email = $("#email").val();
+    result.text("");
+    if(validateEmail(email)) {
+      result.text(email + " is valid");
+      result.css("color", "blue");
+    } else {
+      result.text(email + " is not valid");
+      result.css("color", "red");
+    }
+    return false;
+  }
+  $("#validate").on("click", validate);
